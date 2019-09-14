9d5013a81282d40cc50861e1f52172b9f04b44ec
Maksym Kulibaba <maksymkulibaba@Maksyms-MacBook-Pro.local>
2019-09-14 16:26:28 +0300

    feat(setup): add husky, eslint and prettier config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" id="24343">
         SENTENCES CONSTRUCTOR <small>0.1.0</small>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
