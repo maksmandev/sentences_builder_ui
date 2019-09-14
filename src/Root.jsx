d49f7d014aab7a73df3be5b945da08af2b8d68c6
Maksym Kulibaba <maksymkulibaba@Maksyms-MacBook-Pro.local>
2019-09-14 17:25:44 +0300

    feat(setup): add material ui, node sass

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import './App.css'
+
+import './Root.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" id="24343">
+      <header className="App-header">
         SENTENCES CONSTRUCTOR <small>0.1.0</small>
       </header>
     </div>
